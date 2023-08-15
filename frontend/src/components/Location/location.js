3d64e65e66a5299a74ef3d0468dfa830497ee610
saira-yeasmin-333 <sairayeasminsa@gmail.com>
2023-08-16 00:53:02 +0600

    notification done

--- frontend/src/components/Location/location.js
+++ frontend/src/components/Location/location.js
@@ -1,156 +1,66 @@
-import { useEffect, useMemo,useState } from "react";
+import { useRef,useState } from "react";
 import {TextField,Button} from '@mui/material';
-import axios from 'axios';
-import {setLoading, showToast} from "../../App";
-var mapOptions;
-var map;
-var plusCode = "R85H+MJ"; // Replace this with the plus code you received
-const apiKey = 'AIzaSyB3h0X_Z41DnMPduk98Uhwrvoumlz4yMDM';
-const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&address=${plusCode}`;
+import {setLoading, showError, showSuccess, showToast} from "../../App";
+import MapComponent from "./MapComponent";
+import { saveTutorLocation } from "../../actions/location";
 
 
 function Location() {
-  const center = useMemo(() => ({ lat: 23.7983, lng: 90.3876 }), []);
-  const [location, setLocation] = useState(null);
-  const [placename, setplacename] = useState(null);
+
   const [rad, setR] = useState(null);
+  const mapComponentRef=useRef()
 
   const handleRadiusCHange = (event) => {
     setR(event.target.value);
   };
 
-  const findPlusCode = (map, latLng) => {
-    const geocoder = new window.google.maps.Geocoder();
-    geocoder.geocode({ location: latLng }, (results, status) => {
-        if (status === 'OK') {
-            if (results[0]) {
-                const plusCode = results[0].plus_code.compound_code;
-                console.log("Plus Code:", plusCode);
-
-                const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&address=${plusCode}`;
-
-                fetch(apiUrl)
-                  .then(response => response.json())
-                  .then(data => {
-                    if (data.status === 'OK') {
-                      const result = data.results[0];
-                      const formattedAddress = result.formatted_address;
-                      console.log("Formatted Address:", formattedAddress);
-                      setplacename(formattedAddress)
-                      // Now you have the human-readable address, which may include local place names
-                    } else {
-                      console.error('Geocoding API request failed:', data.status);
-                    }
-                  })
-                  .catch(error => {
-                    console.error('Error fetching geocoding data:', error);
-                  });
-                // Now you can use the plus code as needed
-            } else {
-                console.error('No results found');
-            }
-        } else {
-            console.error('Geocoding request failed:', status);
-        }
-    });
-  };
-
-
-
-  useEffect(()=>{
-    var location = new window.google.maps.LatLng(23.8103, 90.4125)
-    mapOptions = {
-        zoom: 12,
-        center: location,
-        mapTypeId: window.google.maps.MapTypeId.RoadMap
-    }
-    map = new window.google.maps.Map(document.getElementById('map-canvas'), mapOptions)
-    const geocoder = new window.google.maps.Geocoder();
-    const infowindow = new window.google.maps.InfoWindow();
-    const myLatlng = { lat: 23.822350, lng: 90.365417 };
-    
-
-    let infoWindow = new window.google.maps.InfoWindow({
-        // content: "Click the map to get Lat/Lng!",
-        position: myLatlng,
-    });
-    
-    infoWindow.open(map);
-    // Configure the click listener.
-    map.addListener("click", (mapsMouseEvent) => {
-      // Close the current InfoWindow.
-      infoWindow.close();
-      // Create a new InfoWindow.
-      infoWindow = new window.google.maps.InfoWindow({
-        position: mapsMouseEvent.latLng,
-      });
-      console.log("select place :",mapsMouseEvent.latLng.toJSON())
-      setLocation(mapsMouseEvent.latLng.toJSON())
-
-      geocoder
-      .geocode({ location: mapsMouseEvent.latLng })
-      .then((response) => {
-        if (response.results[0]) {
-          console.log('results form: ',response.results[0])
-          infoWindow.setContent(response.results[0].formatted_address);
-        } else {
-          window.alert("No results found");
-        }
-      })
-      .catch((e) => window.alert("Geocoder failed due to: " + e));
-
-      infoWindow.open(map);
-    });
-  },[])
-
-  
-  
-
-
-
  const setRadius=async ()=>{
-    console.log(`befor creating location :,${location}`)
-    if(location && rad){
+    if(mapComponentRef.current.isSelected()){
+      const selectedLocation=mapComponentRef.current.getData()
+      console.log(selectedLocation)
+      if(rad){
         const data = {
-            latitude:location.lat,
-            longitude:location.lng,
-            tutor_id:6,
-            address:"dont know",
-            radius:rad
+          latitude:selectedLocation.location.lat,
+          longitude:selectedLocation.location.lng,
+          address:selectedLocation.placeName,
+          radius:rad
         }
-
+        console.log('data: ',data)
         setLoading(true)
-        axios.post('http://localhost:5000/api/location', data).then(res=>{
-            console.log(res.data)
-            showToast('location added successfully')
-        }).catch(err=>{
-        console.log(err)
-
-        }).finally(()=>{
-            setLoading(false)
-        })
-    } 
+        var res=await saveTutorLocation(data)
+        setLoading(false)
+        if(res)
+          showSuccess("Location saved successfully")
+        else showError("An error occurred")
+      }
+    }else{
+      showError("Please select a location")
+    }
  }
-
-// Converts numeric degrees to radians
-
-
- const findTutionsClick=async ()=>{
-
- }
-
-
   return (
     <div>
-        <h2>
-            Map hi
-        </h2>
-        <div style={{width:"100%"}}>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" value={rad} 
-            onChange={handleRadiusCHange} style={{float:"right"}}/>
+        <div style={{width:"100%",display:'flex',justifyContent:'space-between'}}>
+          <h2 style={{marginLeft:'5vw'}}>
+              Select your preferred location
+          </h2>
+          <div style={{marginTop:'10px',marginRight:'5vw'}}>
+              <TextField id="outlined-basic" label="radius" variant="outlined" value={rad} 
+              onChange={handleRadiusCHange}/>
+          </div>
         </div>
-        <div id="map-canvas" style={{height: "70vh", width: "100vw"}}></div>
-        <div className = "add">
+        
+        <center style={{width:'100%'}}>
+          <MapComponent 
+              ref={mapComponentRef}
+              height="70vh" 
+              width="90vw" 
+              initLocation={{lat: 23.7983, lng: 90.3876}} 
+              initZoom={12}
+              mapId="main-map"
+            />
+        </center>
+        
+        <div className = "add" style={{width:'100%',marginTop:'20px',display:'flex',justifyContent:'center'}}>
             <Button variant={"contained"} onClick={setRadius} style={{borderRadius:15}}> Set Location</Button>
         </div>
     </div>
--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -1,28 +1,28 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Paper from "@mui/material/Paper";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
-import axios from 'axios';
 import {calculateDistance} from '../components/common/distance'
-import Cookies from 'universal-cookie';
-import { showError, showToast } from '../App';
-const cookies = new Cookies();
+import { showError, showSuccess, showToast } from '../App';
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import MapComponent from '../components/Location/MapComponent';
+import { sendNotificationCall } from '../actions/notification';
+import { getAllPreferredLocations } from '../actions/location';
 
 const platforms = [
   "offline",
   "online",
   "both"
 ];
-
-
 
 const CreatePost = () => {
   const [classes, setClasses] = useState([]);
@@ -34,6 +34,11 @@ const CreatePost = () => {
   const [budget, setBudget] =useState("");
   const [platform, setPlatform] = useState("");
 
+  const mapComponentRef=useRef()
+  const [mapDialog,setMapDialog]=useState(false)
+  const [mapData,setMapData]=useState(null)
+  const [locations,setLocations]=useState(null)
+
   const changeClass = (event) => {
     setClass(event.target.value);
   };
@@ -42,61 +47,129 @@ const CreatePost = () => {
     setSubject(event.target.value);
   };
 
-  const [locations,setLocations]=useState([])
-  const [place,setPlace]=useState({ lat: 23.7983, lng: 90.3876 })
-
-  const sendNotification=(tutors)=>{
-    tutors.forEach(t=>{
-      const body={
-        from:0,
-        to:t,
-        message:"new tuition post in location lat: ${place.lat},long: ${place.lng}",
-        is_read:false
-      }
-
-      var res=axios.post('http://localhost:5000/api/notification',{})
-      console.log(res)
-      if(res.data.success){
-        showToast('working')
-      }else{
-        showError('somethins is wrong')
-      }
-    })
+  const sendNotification=async(t)=>{
+    const body={
+      to:t,
+      message:`new tuition post in location lat: ${mapData.placeName}`,
+      is_read:false
+    }
+    // console.log('bpdy : ',body)
+    try{
+      var res=await sendNotificationCall(body)
+      // console.log(res)
+      return res
+      
+    }catch(Err){
+      showError('An error occured')
+    }
   }
 
-  const checkLocations=()=>{
-    axios
-    .get(`http://localhost:5000/api/location`)
-    .then((response) => {
-        const tutors= new Set();
-        setLocations(response.data.data); // Set the fetched data to the state
-        console.log('we get response : ',response.data.data)
-        response.data.data.map((location) => {
-          const distance=calculateDistance(location.latitude,location.longitude,place.lat,place.lng)
-          console.log('dist,rad,: ',distance,location.radius)
-          if (distance<=location.radius){
-            console.log('tutor id: ',location.tutor_id)
-            tutors.add(location.tutor_id)
+  const checkLocations=async()=>{
+    if(mapData){
+      var response=await getAllPreferredLocations()
+      // console.log('we get response : ',response)
+      // setLocations(response);
+      const tutors= new Set();
+      var notificationPromises=[]
+      response.data.map((l)=>{
+        // console.log('entered')
+        // console.log('tutors: ',tutors)
+        if(tutors.has(l.tutor_id)===false)
+        {
+          const distance=calculateDistance(l.latitude,l.longitude,mapData.location.lat,mapData.location.lng)
+          console.log('dist,rad,: ',distance,l.radius)
+          if (distance<=l.radius){
+            tutors.add(l.tutor_id)
+            console.log('tutor id: ',l.tutor_id)
+            // console.log()
+            notificationPromises.push(sendNotification(l.tutor_id))
           }
-        })
-        console.log('cnt: ',tutors.size)
+        }
+      })
 
-        sendNotification(tutors)
-    })
-    .catch((error) => {
-        console.error('Error fetching user profile:', error);
-    });
+      var allResponses=await Promise.all(notificationPromises)
+
+      console.log(allResponses)
+
+
+      // response.data.map((l) => {
+      //   console.log('entered')
+      //   console.log('tutors: ',tutors)
+      //   if(tutors.has(l.tutor_id)===false)
+      //   {
+      //     const distance=calculateDistance(l.latitude,l.longitude,mapData.location.lat,mapData.location.lng)
+      //     // console.log('dist,rad,: ',distance,l.radius)
+      //     if (distance<=l.radius){
+      //       tutors.add(l.tutor_id)
+      //       // console.log('tutor id: ',l.tutor_id)
+      //       sendNotification(l.tutor_id)
+      //     }
+      //   }
+      // })
+    }else{
+      showError('Please select your location')
+    }  
+    
   }
 
   //---------------------------------------------here is my portion
-  useEffect(()=>{
-    
-  })
+
+
+
+  const dialogSaveClick=async ()=>{
+      if(mapComponentRef.current.isSelected()){
+        var mapData=mapComponentRef.current.getData()
+        console.log(mapComponentRef.current)
+        setMapData(mapData)
+        setMapDialog(false)
+        showSuccess("Location selected")
+      }else
+        showError("Please select a location")
+  }
 
   
 
   return (
     <div>
+      <Dialog
+        open={mapDialog}
+        >
+          <DialogTitle>
+            Select Your Location
+          </DialogTitle>
+
+          <DialogContent>
+
+          <center style={{width:'100%'}}>
+            <MapComponent
+                ref={mapComponentRef}
+                height="55vh" 
+                width="40vw" 
+                initLocation={{lat: 23.7983, lng: 90.3876}} 
+                initZoom={12}
+                mapId="post-map"
+              />
+          </center>
+
+          </DialogContent>
+          <DialogActions>
+            <Button
+            onClick={()=>{
+              setMapDialog(false)
+              setMapData(null)
+            }}
+              color='error'
+              >
+                Close
+              </Button>
+              <Button
+              onClick={dialogSaveClick}
+              color='primary'
+              >
+                Save
+              </Button>
+          </DialogActions>
+      </Dialog>
       <Paper elevation={3} sx={{ marginRight: "15%", marginLeft: "15%" }}>
         <Box sx={{ padding: 5 }}>
             <Typography variant="h6" gutterBottom sx={{ color: "#0067AB", paddingBottom: 5 }}>
@@ -210,8 +283,19 @@ const CreatePost = () => {
               </FormControl>
             </Grid>
 
+              <div id="location-icon" style={{marginTop:'20px',display:'flex',flexDirection:'row-reverse',width:'100vw'}}>
+                <Button
+                  variant='contained'
+                  onClick={()=>{
+                    setMapDialog(true)
+                  }}
+                  startIcon={<LocationOnIcon/>}
+                  >
+                    Set Location
+                  </Button>
+              </div>
+            
 
-            <Grid item xs={12} sm={5} />
             <Grid item xs={12} sm={5} />
             <Grid item xs={12} sm={12}>
                 <Button variant="contained" sx={{ justifyContent: "center" }} onClick={checkLocations}>
