464b887870ccf55bd48966e085a82d621f323bf3
SanjanaSiraj <sanjanasiraj412@gmail.com>
2023-08-21 00:33:05 +0600

    texteditor and other designs

--- frontend/src/pages/profilePage.js
+++ frontend/src/pages/profilePage.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Avatar, Button, Card, CardContent, CardHeader, Tab, Tabs, Rating } from '@mui/material';
+import { Avatar, Button, Card, CardContent, CardHeader, Tab, Tabs, Rating, IconButton } from '@mui/material';
 import PrimarySearchAppBar from '../components/Appbar/appbar';
 import Efficiencies from './Efficiencies';
 import Cookies from 'universal-cookie';
@@ -9,6 +9,8 @@ import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase/firebase";
 import { v4 } from "uuid";
 import { setLoading, showToast } from '../App';
+import EditIcon from '@mui/icons-material/Edit';
+import TextEditor from '../components/common/TextEditor';
 
 const cookies = new Cookies();
 
@@ -19,10 +21,13 @@ const ProfilePage = () => {
     const [imageUpload, setImageUpload] = useState(null);
     const [imageUrls, setImageUrls] = useState(null);
     const imageRef = ref(storage, `images/${v4()}${imageUpload?.name}`);
+    const [rating, setRating] = useState(0);
+    const [reviews, setReviews] = useState([]);
 
     const handleTabChange = (event, newValue) => {
         setCurrentTab(newValue);
     };
+    
     
 
     // const uploadFile = () => {
@@ -55,6 +60,32 @@ const ProfilePage = () => {
         })
     }
 
+    const handleEditProfileClick = () => {
+        // Navigate to the edit profile page
+        // You need to implement this navigation logic based on your routing setup
+      };
+
+    const fetchUserRating = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/get-rating`,{headers:{authorization:'Bearer '+cookies.get('token')}});
+            setRating(response.data.data);
+            console.log("Rating:")
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching user rating:', error);
+        }
+    };
+
+    const fetchReviews = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/review`,{headers:{authorization:'Bearer '+cookies.get('token')}});
+            setReviews(response.data.data);
+            console.log("Review:")
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching user rating:', error);
+        }
+    }
     
 
     useEffect(() => {
@@ -69,7 +100,10 @@ const ProfilePage = () => {
                     console.log('here entered')
                     setImageUrls(response.data.data.image)
                 }
-                
+                if (response.data.data.role === 'teacher') {
+                    fetchUserRating();
+                    fetchReviews();
+                }
             })
             .catch((error) => {
                 console.error('Error fetching user profile:', error);
@@ -85,7 +119,7 @@ const ProfilePage = () => {
         <div style={{ backgroundColor: '#BDCDF5', minHeight: '100vh' }}>
           <PrimarySearchAppBar type={user.type} />
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px' }}>
-            <Card sx={{ width: 600, marginTop: '20px', display: 'flex', flexDirection: 'row' }}>
+            <Card sx={{ width: 1200, marginTop: '20px', display: 'flex', flexDirection: 'row' }}>
               <div style={{ flex: 1, padding: '20px' }}>
                 {user.image ? (
                   <img alt="User" src={user.image} style={{ width: '100%', maxHeight: '300px', objectFit: 'cover' }} />
@@ -94,63 +128,99 @@ const ProfilePage = () => {
                 )}
               </div>
               <div style={{ flex: 2, padding: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                <h1 style={{ fontSize: '24px', fontWeight: 'bold', margin: '10px 0' }}>{user.name}</h1>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h1 style={{ fontSize: '24px', fontWeight: 'bold', margin: '10px 0' }}>{user.name}</h1>
+                    <IconButton onClick={handleEditProfileClick} aria-label="Edit Profile">
+                        <EditIcon /> {/* Use the appropriate icon component */}
+                    </IconButton>
+                </div>
                 <p style={{ fontSize: '14px', margin: '0', color: '#007bff' }}>{user.role.charAt(0).toUpperCase() + user.role.slice(1)}</p>
                 <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
-                  <Rating name="half-rating" defaultValue={2.5} precision={0.5} readOnly />
-                  <p style={{ marginLeft: '10px', fontWeight: 'bold', fontSize: '18px', color: 'black' }}>2.5</p>
+                  <Rating name="half-rating" value={rating} precision={0.5} readOnly />
+                  <p style={{ marginLeft: '10px', fontWeight: 'bold', fontSize: '18px', color: 'black' }}>{rating}</p>
                 </div>
     
                 <Tabs value={currentTab} onChange={handleTabChange} style={{ marginTop: '20px' }}>
                   <Tab label="About"  />
-                  <Tab label="Reviews" />
+                  {user.role === "teacher"? <Tab label="Reviews" />: <Tab label="Grades" />}
                   <Tab label="Overview" />
                 </Tabs>
     
-                {currentTab === 0 && (
-                     <CardContent>
-                        <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
-                        Phone: {'\t'} {/* Tabbed space */}
-                        <span style={{ fontSize: '14px', fontWeight: 'normal', color: '#007bff' }}>{user.phone}</span>
-                        </p>
-                        <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
-                        Email: {'\t'} {/* Tabbed space */}
-                        <span style={{ fontSize: '14px', fontWeight: 'normal', color: '#007bff' }}>{user.email}</span>
-                        </p>
-                        <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
-                        Efficiency: {'\t'} {/* Tabbed space */}
-                        <span style={{ fontSize: '14px', fontWeight: 'normal' }}>
-                            {user.subjects.map((sub, i) => (
-                            <span key={i}>{
-                                <span
-                                    key={i}
-                                    style={{
-                                    display: 'inline-block',
-                                    margin: '2px',
-                                    padding: '5px 10px',
-                                    borderRadius: '20px',
-                                    backgroundColor: '#007bff',
-                                    color: 'white',
-                                    }}
-                                >
-                                    {sub.sub_name}
+                <div >
+                    {currentTab === 0 && (
+                        <CardContent style={{ overflowY: 'auto', height: '200px' }}>
+                            <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
+                            Phone: {'\t'} {/* Tabbed space */}
+                            <span style={{ fontSize: '14px', fontWeight: 'normal', color: '#007bff' }}>{user.phone}</span>
+                            </p>
+                            <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
+                            Email: {'\t'} {/* Tabbed space */}
+                            <span style={{ fontSize: '14px', fontWeight: 'normal', color: '#007bff' }}>{user.email}</span>
+                            </p>
+                            {user.role === "teacher" && 
+                                <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
+                                Efficiency: {'\t'} {/* Tabbed space */}
+                                <span style={{ fontSize: '14px', fontWeight: 'normal' }}>
+                                    {user.subjects.map((sub, i) => (
+                                    <span key={i}>{
+                                        <span
+                                            key={i}
+                                            style={{
+                                            display: 'inline-block',
+                                            margin: '2px',
+                                            padding: '5px 10px',
+                                            borderRadius: '20px',
+                                            backgroundColor: '#007bff',
+                                            color: 'white',
+                                            }}
+                                        >
+                                            {sub.sub_name}
+                                        </span>
+                                        }{i < user.subjects.length - 1 ? ', ' : ''}</span>
+                                    ))}
                                 </span>
-                                }{i < user.subjects.length - 1 ? ', ' : ''}</span>
-                            ))}
-                        </span>
-                        </p>
-                   </CardContent>
-                )}
+                                </p>
+                            }
+                    </CardContent>
+                    )}
 
-                {currentTab === 1 && (
-                     <CardContent>
-                        <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
-                        Phone: {'\t'} {/* Tabbed space */}
-                        <span style={{ fontSize: '14px', fontWeight: 'normal', color: '#007bff' }}>{user.phone}</span>
-                        </p>
-                   </CardContent>
-                )}
+                    {currentTab === 1 && (
+                        <CardContent style={{ overflowY: 'auto', height: '200px' }}>
+                        {reviews.length > 0 ? (
+                          reviews.map((review, index) => (
+                            <Card key={index} style={{ marginBottom: '10px', padding: '10px' }}>
+                              <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <Avatar alt="User" src={review.account.image} />
+                                <div style={{ marginLeft: '10px' }}>
+                                  <h4 style={{ fontSize: '16px', fontWeight: 'bold', margin: '0' }}>{review.account.name}</h4>
+                                  <p style={{ fontSize: '14px', margin: '0' }}>{new Date(review.timestamp).toLocaleString()}</p>
+                                </div>
+                              </div>
+                              <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+                                <Rating name={`review-rating-${index}`} value={review.rating} precision={0.5} readOnly />
+                                <h4 style={{ fontSize: '16px', fontWeight: 'bold', margin: '0', marginLeft: '10px' }}>{review.rating}</h4>
+                              </div>
+                              <div>
+                                <TextEditor review={review.review_text} readonly_val={true} />
+                              </div>
+                            </Card>
+                          ))
+                        ) : (
+                          <p>No reviews available.</p>
+                        )}
+                        </CardContent>
+                    )}
 
+                    {currentTab === 2 && (
+                        <CardContent style={{ overflowY: 'auto',height: '200px' }}>
+                            <p style={{ margin: '5px 0', fontSize: '16px', fontWeight: 'bold' }}>
+                            Phone: {'\t'} {/* Tabbed space */}
+                            <span style={{ fontSize: '14px', fontWeight: 'normal', color: '#007bff' }}>{user.phone}</span>
+                            </p>
+                    </CardContent>
+                    )}
+
+                </div>
     
                 {/* Add content for other tabs here */}
               </div>
