d49fc9bab44b2e6d38c5af6445e44b1e7319fd5d
SanjanaSiraj <sanjanasiraj412@gmail.com>
2023-08-21 11:22:46 +0600

    major design changes

--- backend/controllers/location.js
+++ backend/controllers/location.js
@@ -23,6 +23,31 @@ class LocationController extends Controller{
             data:result
         })
     }
+
+    delete = async(req,res)=>{
+        const locationIdToDelete = req.body.location_id; // Assuming you're passing the location ID in the request parameter
+
+        try {
+        const deleteResult = await locationService.delete(locationIdToDelete);
+        if (deleteResult) {
+            return res.json({
+            success: true,
+            message: "Location deleted successfully",
+            });
+        } else {
+            return res.status(404).json({
+            success: false,
+            message: "Location not found",
+            });
+        }
+        } catch (error) {
+        console.error("Error deleting location:", error);
+        return res.status(500).json({
+            success: false,
+            message: "An error occurred while deleting the location",
+        });
+        }
+    }
 }
 
 module.exports = {LocationController}
