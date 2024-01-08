2a139b5f0e9c4e8a25d7db9d1b787ac8abb09ecd
saira-yeasmin-333 <69497719+saira-yeasmin-333@users.noreply.github.com>
2024-01-08 12:27:40 +0600

    Update firebase.js

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage";
 // Set the configuration for your app
 // TODO: Replace with your app's config object
 const firebaseConfig = {
-    apiKey: "AIzaSyCdc6PjgWAcKCg8D1p6Doprdq53SZt1kxY",
-    authDomain: "tutor-hub-4b86c.firebaseapp.com",
-    projectId: "tutor-hub-4b86c",
-    storageBucket: "tutor-hub-4b86c.appspot.com",
-    messagingSenderId: "726108214056",
-    appId: "1:726108214056:web:299dd0459c3ceb6de8761d",
-    measurementId: "G-B4WXKZ0R7T"
+    //configObject
 };
 const firebaseApp = initializeApp(firebaseConfig);
 
